a5c9ba36ca20d21e1e295f0c099d4e698e568d61
정훈박 <fireking5997@gmail.com>
2022-06-24 10:27:47 +0900

    add:readme 추가

--- READMD.md
+++ READMD.md
@@ -1,1 +1,27 @@
 그냥 생각없이 인강 보고 따라한다 막히면 DOC 보고 해결하면서 하고 있음.
+
+## Math.atan2(a,b)
+
+- 라디안을 구해줌
+- 인자(a: y 거리차이,b : x 거리차이 )
+  - 거리는 현 위치 기준이다.
+    - 중앙에서 구석으로 가려면 구석 - 중앙
+    - 구석에서 중앙으로 오려면 중앙 - 구석
+    -
+
+## canvas.arc(x,y,radius,start,end,option)
+
+- 캔버스에 원을 그려줌
+- 인자(x:x좌표, y:y,좌표, radius:반지름, start:원시작, end:원 종료, option:이건 모르겠다..)
+  ```
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+  ```
+
+## 라디안 _ cos = 그 방향 y 좌표, 가디안 _ sin = 그 방향 x 좌표
+
+```
+  const velocity = {
+    x: Math.cos(angle),
+    y: Math.sin(angle),
+  };
+```
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-"use strict";
-
 const canvas = document.querySelector("canvas");
 
 canvas.width = window.innerWidth;
@@ -27,10 +25,76 @@ class Player {
   }
 }
 
+class P extends Player {
+  constructor({ x, y, radius, color, velocity }) {
+    super({ x, y, radius, color });
+    this.velocity = velocity;
+  }
+
+  update() {
+    this.draw();
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+  }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
-const player = new Player({ x: x, y: y, radius: 30, color: "blue" });
-player.draw();
+const player = new Player({ x, y, radius: 30, color: "blue" });
+const ps = [];
 
-console.log(player);
+const es = [];
+
+function addEnemy() {
+  setInterval(() => {
+    const r = Math.atan2(canvas.height / 2 - 100, canvas.width / 2 - 100);
+    es.push(
+      new P({
+        x: 100,
+        y: 100,
+        radius: 30,
+        velocity: { x: Math.cos(r), y: Math.sin(r) },
+      })
+    );
+  }, 1000);
+}
+
+addEnemy();
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  player.draw();
+  for (const p of ps) {
+    p.update();
+  }
+
+  for (const e of es) {
+    e.update();
+  }
+}
+
+window.addEventListener("click", (event) => {
+  const angle = Math.atan2(
+    event.clientY - canvas.height / 2,
+    event.clientX - canvas.width / 2
+  );
+
+  const velocity = {
+    x: Math.cos(angle),
+    y: Math.sin(angle),
+  };
+
+  const p = new P({
+    x,
+    y,
+    radius: 10,
+    color: "green",
+    velocity,
+  });
+  console.log(angle);
+  ps.push(p);
+});
+
+animate();
